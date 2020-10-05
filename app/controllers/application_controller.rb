d9ad2e5573bf584d089e65cf5b90e1027d224eac
Uduak Essien <acushla4real@gmail.com>
2020-10-05 17:00:18 +0100

    update auth layout template for devise users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def layout_by_resource
-    if devise_controller? && resource_name == :user && action_name == 'new'
+    if devise_controller? && resource_name == :user
       'auth'
     else
       'application'
